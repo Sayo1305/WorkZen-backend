4306f68c9f3378d2e4a5268ab8d0961b2094acd4
Unnat Das <udas4153@gmail.com>
2023-11-23 04:36:59 +0530

    added sign in api

--- model/User.js
+++ model/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  Name: {
+  name: {
     type: String,
     required: true,
   },
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const express = require('express');
 const User = require('../model/User');
 const router = express.Router();
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
 
 
 const hashPassword = async(password) => {
@@ -12,28 +14,72 @@ const hashPassword = async(password) => {
   return hashedValue;
 }
 
+const validatePassword = (password , hash ) => {
+  const hashPass = crypto.createHash('sha256');
+  hashPass.update(password);
+  const hashedValue = hash.digest('hex');
+  return hashedValue === hash;
+}
+
+const getUserId  = (token) => {
+  const userid = jwt.verify(token , process.env.SECRET_KEY);
+  return userid?.userId
+} 
+
 router.post('/sign_up' , async (req , res) => { 
   try{
     // using Google Id
     if(req.body.google_id){
+      const {email  , name ,google_id , profile_url} = req.body;
+      const already_user = await User.find({email  : email});
+      if(already_user && already_user.length > 0){
+        return res.status(400).json({ok : false , msg : "Already User Exist"});
+      }
+      const user_id = Date.now().toString().substring(6);
+      const newUser = new User({
+        email: email,
+        name: name,
+        user_id: user_id,
+        google_id : google_id,
+        profile_url : profile_url
+      });
+      await newUser.save();
+      const hashToken = jwt.sign({userId: user_id} , process.env.SECRET_KEY);
+      return res.status(201).json({ ok: true, msg: "User created successfully", token: hashToken });
 
     }else{
-      const {email  , password , name , role  , teamDetails} = req.body;
+      const {email  , password , name } = req.body;
       // check if email already exist
-      const already_user = await User.find({email  : email});
-      if(already_user){
-        return res.status(400).json({ok : false , msg : "Already User Exist"});
+      const already_user = await User.find({email  : email}).lean().exec();
+      if(already_user && already_user.length > 0){
+        return res.status(400).send({ok : false , msg : "Already User Exist"});
       }
       const hashpass = hashPassword(password);
       const user_id = Date.now().toString().substring(6);
-      if(!teamDetails){
-        return res.status(400).json({ok : false , msg : "Organisation is not setup"});
-      }
+      const newUser = new User({
+        email: email,
+        password: hashpass?.Promise,
+        name: name,
+        user_id: user_id,
+      });
+      await newUser.save();
+      const hashToken = jwt.sign({userId: user_id} , process.env.SECRET_KEY);
+      return res.status(201).json({ ok: true, msg: "User created successfully", token: hashToken });
     }
   }catch(err){
+    console.log(err);
     return  res.status(500).json({ok : false , msg  : err});
   }
 })
+
+
+
+
+// make for login here
+
+router.post('/login', async(req, res)=>{
+
+});
 
 router.get('/users', async (req, res) => {
   try {
